bf9650d34af14f3914fdbbe0099696df85f6ec3a
Chetan Verma <chetan61verma@gmail.com>
2025-07-23 10:03:46 +0530

    updates

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
+import "./global.css";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <StatusBar />
+      <Stack />
+    </SafeAreaProvider>
+  );
 }
